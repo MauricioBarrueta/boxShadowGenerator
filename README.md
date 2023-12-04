c4e9136b991472fcf43f6449363f65bca350b1ca
Mauricio <mauba22@outlook.com>
2023-12-04 13:47:55 -0600

    Updates

---
+++ README.md
@@ -0,0 +1,7 @@
+🇲🇽 Este sitio web permite generar-personalizar los valores de la propiedad 'box-shadow' de CSS, el usuario manipula los valores mientras al mismo tiempo se muestra la vista previa 
+   del estilo, además de que genera el código con la sintaxis de la propiedad para poder copiarlo.
+
+🇺🇸 This website allows you to generate-customize the values ​​of the CSS 'box-shadow' property, the user manipulates the values ​​while displaying the preview of the style, also 
+   generates the code with the syntax of the property in order to copy it.
+
+![boxShadow](https://github.com/MauricioBarrueta/boxShadowGenerator/assets/60496232/5b8540cb-870c-4643-84f0-ab9d4be481ea)
--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,31 @@
 const hOffset = document.getElementById('hOffset'), vOffset = document.getElementById('vOffset'), blurRadius = document.getElementById('blurRadius'), 
     spread = document.getElementById('spread'), opacity = document.getElementById('opacity'), borderRadius = document.getElementById('border')
-const hOffsetValue = document.getElementById('hRange'), bRadiusValue = document.getElementById('blurRange'), 
-    opValue = document.getElementById('opaRange'), vOffsetValue = document.getElementById('vRange'), 
-    spRadiusValue = document.getElementById('spreadRange'), borderValue = document.getElementById('radiusRange')
+const hOffsetValue = document.getElementById('hRange'), opValue = document.getElementById('opaRange'), 
+    vOffsetValue = document.getElementById('vRange'), borderValue = document.getElementById('radiusRange')
+const colorShadow = document.getElementById('color').value, insetShadow = document.getElementById('inset')
+
+const generatedBoxShadow = document.querySelector('.box-shadow-preview'), boxShadowCode = document.getElementById('bs-code'),
+    boxShadowOptions = document.querySelectorAll('.shadow-options input')
 
 window.onload = () => {
     resetRangeValues()  
     generateOrUpdateBoxShadow()
 }
 
-/* Resetea los valores de los rangos */
-const resetRangeValues = () => {
-    hOffsetValue.value = '5', hOffset.value = 5, vOffsetValue.value = '5', vOffset.value = 5
-    bRadiusValue.value = '0', blurRadius.value = 0, spRadiusValue.value = '0', spread.value = 0, borderValue.value = '0', borderRadius.value = 0
-    opValue.value = '0.75', opacity.value = 0.75      
-}
-const boxShadowOptions = document.querySelectorAll('.shadow-options input')
-/* Se llama a la función cada que cambia el rango de alguna opción */
+/* Se llama a la función cada que el valor de los input tipo 'range' y el checkbox cambian */
 boxShadowOptions.forEach((range) => {
     range.addEventListener('input', generateOrUpdateBoxShadow)
 });
+insetShadow.addEventListener('change', () => { generateOrUpdateBoxShadow() })
+
+/* Genera la vista previa y el código de box-shadow */
+function generateOrUpdateBoxShadow() {
+    const code = `${insetShadow.checked ? 'inset ' : ''} ${hOffset.value}px ${vOffset.value}px ${blurRadius.value}px ${spread.value}px ${hexColorToRgba(colorShadow, opacity.value)}`
+
+    generatedBoxShadow.style.boxShadow = code
+    generatedBoxShadow.style.borderRadius = `${borderRadius.value}%`
+    boxShadowCode.textContent = `box-shadow: ${code};\n-webkit-box-shadow: ${code};\n-moz-box-shadow: ${code};\n\nborder-radius: ${borderRadius.value}%;`
+}
 
 /* Transforma el color hexadecimal al modelo RGB */
 const hexColorToRgba = (colorShw, opacityShw) => {
@@ -29,23 +35,17 @@ const hexColorToRgba = (colorShw, opacityShw) => {
     return `rgba(${r}, ${g}, ${b}, ${opacityShw})`
 }
 
-const generatedBoxShadow = document.querySelector('.box-shadow-preview'), boxShadowCode = document.getElementById('bs-code')
-/* Genera la vista previa y el código de box-shadow */
-function generateOrUpdateBoxShadow() {
-    const colorShadow = document.getElementById('color').value, insetShadow = document.getElementById('inset').checked
-    /* Se crea la estructura del código */
-    const code = `${insetShadow ? 'inset ' : ''} ${hOffset.value}px ${vOffset.value}px ${blurRadius.value}px ${spread.value}px ${hexColorToRgba(colorShadow, opacity.value)}`
-    /* Se le asignan los estilos a la figura de vista previa y se inserta el código en el área de texto */
-    generatedBoxShadow.style.boxShadow = code
-    generatedBoxShadow.style.borderRadius = `${borderRadius.value}px`
-    boxShadowCode.textContent = `-webkit-box-shadow: ${code}; \n-moz-box-shadow: ${code}; \nbox-shadow: ${code};\nborder-radius: ${borderRadius.value}px;`
-}
-generateOrUpdateBoxShadow()
-
-/* Copia todas las lineas de código */
 const copyCodeBtn = document.querySelector('.copy-btn')
 copyCodeBtn.addEventListener('click', () => {
     boxShadowCode.select()
     boxShadowCode.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(boxShadowCode.value)
 })
+
+/* Resetea los valores de los rangos */
+const resetRangeValues = () => {
+    boxShadowOptions.forEach(element => { element.value = 0 });
+    hOffsetValue.value = hOffset.value = vOffsetValue.value = vOffset.value = borderRadius.value = borderValue.value = 5
+    opValue.value = opacity.value = 0.75     
+    insetShadow.checked = false
+}
